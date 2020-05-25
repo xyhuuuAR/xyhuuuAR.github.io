0d9b269424555e78811b8c056be5b5cdc03fbaa3
Xiaoyun Hu <xiaoyunhu0812@gmail.com>
2020-05-25 17:13:01 +1000

    Update Arrow.js

--- Scripts/Arrow.js
+++ Scripts/Arrow.js
@@ -1,22 +1,23 @@
-var pathtriangle = d3.select("#" + "triangle");
+window.onload = function () {
+    var pathtriangle = d3.select("#" + "triangle");
 
-function handleDirection(event){
-    var dir = Math.round(event.alpha);
+    function handleDirection(event) {
+        var dir = Math.round(event.alpha);
 
-    if (dir >= 270 && dir < 360){
-        pathtriangle.transition()
+        if (dir >= 43 && dir < 46) {
+            pathtriangle.transition()
                 .duration(500)
                 .ease(d3.easeLinear)
                 .attr('fill', 'green')
 
-    } else {
-        pathtriangle.transition()
+        } else {
+            pathtriangle.transition()
                 .duration(500)
                 .ease(d3.easeLinear)
                 .attr('fill', 'blue')
+        }
+
     }
-   
+
+    window.addEventListener('deviceorientation', handleDirection);
 }
-
-window.addEventListener('deviceorientation', handleDirection);
-
