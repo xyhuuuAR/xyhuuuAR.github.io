81f6a624f104581659a12887545efa9bc49e9df2
Xiaoyun Hu <xiaoyunhu0812@gmail.com>
2020-05-25 13:01:30 +1000

    Update DeviceOrientationHandler.js

--- Scripts/DeviceOrientationHandler.js
+++ Scripts/DeviceOrientationHandler.js
@@ -3,25 +3,6 @@ function deviceOrientationHandler(tiltLR, tiltFB, dir){
     document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
     document.getElementById("doDirection").innerHTML = Math.round(dir);
 
-    //Apply the transform to the image
-    // var logo = document.getElementById("imgLogo");
-    var logo = document.getElementById("box");
-    // logo.style.webkitTransform =
-    //     "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
-    // logo.style.MozTransform = "rotate(" + tiltLR + "deg)";
-    // logo.style.transform =
-    //     "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
-
-    logo.object3D.transform =
-        "rotateZ(" + (dir- 180) + "deg) " +
-        // "rotateZ(" + (dir) + "deg) " +
-        "rotateX(" + tiltFB + "deg) " +
-        "rotateY(" + (-tiltLR) + "deg)";
-        // "rotateX(" + tiltFB + "deg) " +
-        // "rotateY(" + (-tiltLR) + "deg)";
-
-
-
 }
 
 function requestOrientationPermission() {
@@ -30,25 +11,7 @@ function requestOrientationPermission() {
         alert("Orientation access status: " + permissionState);
         if (permissionState === 'granted') {
             if (window.DeviceOrientationEvent) {
-                // console.log("DeviceOrientation is supported.");
-                document.getElementById("doEvent").innerHTML = "DeviceOrientation";
-                // Listen for the deviceorientation event and handle the raw data
-                window.addEventListener('deviceorientation', function(eventData) {
-                    // gamma is the left-to-right tilt in degrees, where right is positive
-                    var tiltLR = eventData.gamma;
-    
-                    // beta is the front-to-back tilt in degrees, where front is positive
-                    var tiltFB = eventData.beta;
-    
-                    // alpha is the compass direction the device is facing in degrees
-                    var dir = eventData.alpha
-    
-                    // call our orientation event handler
-                    deviceOrientationHandler(tiltLR, tiltFB, dir);
-    
-                }, false);
             } else {
-                document.getElementById("doEvent").innerHTML = "Not supported."
             } 
         }
     }).catch(error => {
@@ -59,23 +22,6 @@ function requestOrientationPermission() {
     {
         // alert("window.DeviceOrientationEvent: " + window.DeviceOrientationEvent);
         if (window.DeviceOrientationEvent) {
-            console.log("DeviceOrientation is supported.");
-            document.getElementById("doEvent").innerHTML = "DeviceOrientation";
-            // Listen for the deviceorientation event and handle the raw data
-            window.addEventListener('deviceorientation', function(eventData) {
-                // gamma is the left-to-right tilt in degrees, where right is positive
-                var tiltLR = eventData.gamma;
-
-                // beta is the front-to-back tilt in degrees, where front is positive
-                var tiltFB = eventData.beta;
-
-                // alpha is the compass direction the device is facing in degrees
-                var dir = eventData.alpha
-
-                // call our orientation event handler
-                deviceOrientationHandler(tiltLR, tiltFB, dir);
-
-            }, false);
         }
     }
   }
