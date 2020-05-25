9c1d7f01b2405571014d706c509421167990637d
Xiaoyun Hu <xiaoyunhu0812@gmail.com>
2020-05-25 13:13:23 +1000

    Update Arrow.js

--- Scripts/Arrow.js
+++ Scripts/Arrow.js
@@ -19,7 +19,7 @@ window.onload = function(){
                      { "x": 25,  "y": 30}];
     
     //This is the accessor function we talked about above
-    var lineFunction = svgbigbluearrow.line()
+    var lineFunction = d3.svg.line()
                              .x(function(d) { return d.x; })
                              .y(function(d) { return d.y; })
                              .interpolate("linear");
