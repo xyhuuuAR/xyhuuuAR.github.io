7b3e5c73c35f7ddc32bb94671e65648e6e70036a
Xiaoyun Hu <xiaoyunhu0812@gmail.com>
2020-05-25 15:00:56 +1000

    ss

--- Scripts/Arrow.js
+++ Scripts/Arrow.js
@@ -1,5 +1,22 @@
-window.onload = function(){
+var pathtriangle = d3.select("#" + "triangle");
 
+function handleDirection(event){
+    var dir = Math.round(event.alpha);
+
+    if (dir >= 270 && dir < 360){
+        pathtriangle.transition()
+                .duration(500)
+                .ease(d3.easeLinear)
+                .attr('fill', 'green')
+
+    } else {
+        pathtriangle.transition()
+                .duration(500)
+                .ease(d3.easeLinear)
+                .attr('fill', 'blue')
+    }
    
-
 }
+
+window.addEventListener('deviceorientation', handleDirection);
+
