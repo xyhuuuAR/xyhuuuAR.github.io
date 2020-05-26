d709341eaa52cfeefc51a99b8436b9c5852d2bd0
Xiaoyun Hu <xiaoyunhu0812@gmail.com>
2020-05-26 15:35:52 +1000

    ss

--- Scripts/Arrow.js
+++ Scripts/Arrow.js
@@ -1,5 +1,33 @@
 window.onload = function () {
     var pathtriangle = d3.select("#" + "triangle");
+    var pathArrowUp = d3.select("#" + "arrowup");
+    var pathArrowDown = d3.select("#" + "arrowdown");
+    var circle = d3.select("#" + "circle");
+
+    const cameraButton = document.getElementById('camera--trigger');
+    const directionArrow = document.getElementById('triangle');
+    const positionCircle = document.getElementById('circle');
+    const upArrow = document.getElementById('arrowup');
+    const downArrow = document.getElementById('arrowdown');
+
+    var isGoalRotation = false;
+    var isGoalTilt = false;
+
+    function toggleCameraButton(flag){
+        if (flag){
+            cameraButton.style.visibility = 'visible';
+        } else {
+            cameraButton.style.visibility = 'hidden';
+        }
+    }
+
+    function toggleDirectionArrow(flag){
+        if (flag){
+            directionArrow.style.visibility = 'visible';
+        } else {
+            directionArrow.style.visibility = 'hidden';
+        }
+    }
 
     function handleDirection(event) {
         var dir = Math.round(event.alpha);
